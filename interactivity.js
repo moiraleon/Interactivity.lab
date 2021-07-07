a935f41f88860a8e1d223b80e9a3b872cbd930c7
Moira Leon <86265787+moiraleon@users.noreply.github.com>
2021-07-07 16:04:55 -0600

    Add files via upload

---
+++ interactivity.js
@@ -0,0 +1,42 @@
+console.log("hello world")
+
+let message = document.querySelector("#message");
+
+function addMovie (event){
+    event.preventDefault()
+    let inputField = document.querySelector('input')
+
+    let movie = document.createElement('li')
+    let movieTitle =document.createElement('span');
+    movieTitle.textContent = inputField.value;
+    movie.appendChild(movieTitle);
+    movieTitle.addEventListener('click',crossOffMovie);
+    
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    let list = document.querySelector('ul');
+    list.appendChild(movie);
+    inputField.value= ""
+}
+document.querySelector('form').addEventListener('submit', addMovie)
+
+function deleteMovie (event){
+    event.target.parentNode.remove() 
+    message.textContent = "Movie Deleted!"
+}
+
+function crossOffMovie(event){
+    event.target.classList.toggle('checked') 
+    if
+ (event.target.classList.contains('checked')===true){
+        message.textContent = 'Movie Watched!'
+
+    }
+    else
+    { message.textContent = 'Movie added back!'
+
+    }
+}
